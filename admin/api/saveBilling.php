b67af5d6ae0262a2e1ab0f955c39ef4c475a3c5e
sharmainemanuel <manuelsharmaine@gmail.com>
2018-05-25 02:00:55 +0800

    WORKING
    WORKING

--- admin/api/saveBilling.php
+++ admin/api/saveBilling.php
@@ -7,10 +7,11 @@ $computer = $_POST['computer'];
 $studno = $_POST['studno'];
 $misc = $_POST['misc'];
 $hrm = $_POST['hrm'];
+$sem = $_POST['sem'];
 $totalFee = $_POST['totalFee'];
 
 
-$sql = "INSERT into tbl_billing (id,tuition_fee,registration_fee,lab_fee,comlab_fee,misc_fee,hrm_fee,total_fee,student_no) values ('','$tuition','$registration','$labfee','$computer','$misc','$hrm','$totalFee','$studno')";
+$sql = "INSERT into tbl_billing (id,tuition_fee,registration_fee,lab_fee,comlab_fee,misc_fee,hrm_fee,total_fee,semester,student_no) values ('','$tuition','$registration','$labfee','$computer','$misc','$hrm','$totalFee','$sem','$studno')";
 if (mysqli_query($conn, $sql)) {
     echo "inserted";
 } else {
